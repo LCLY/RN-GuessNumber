59aac477c1547271184c7d8ddf887a810db0d3bb
lchoo <lchoo1996@gmail.com>
2019-10-22 14:28:33 -0700

    refactor: replace view with card component

--- rn-guessnum/screens/StartGameScreen.js
+++ rn-guessnum/screens/StartGameScreen.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { View, StyleSheet, Text, TextInput, Button } from "react-native";
-
+import Card from "../components/Card";
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start a new game</Text>
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
                 <TextInput />
                 <View style={styles.buttonContainer}>
                     <Button title="Reset" onPress={() => {}}></Button>
                     <Button title="Confirm" onPress={() => {}}></Button>
                 </View>
-            </View>
+            </Card>
         </View>
     );
 };
@@ -31,12 +31,6 @@ const styles = StyleSheet.create({
         width: 300,
         maxWidth: "80%",
         alignItems: "center",
-        shadowColor: "grey",
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        elevation //this is for android only
-        backgroundColor: "white",
     },
     buttonContainer: {
         flexDirection: "row",
